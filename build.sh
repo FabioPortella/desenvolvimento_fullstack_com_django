07fc86e80f0320424d0ffb5a375837d4dfd79e97
FabioPortella <fabioportella2009@gmail.com>
2024-10-03 12:06:02 -0300

    create superuser em build

--- build.sh
+++ build.sh
@@ -11,3 +11,6 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# Criando um superuser
+python manage.py createsuperuser --no-input
